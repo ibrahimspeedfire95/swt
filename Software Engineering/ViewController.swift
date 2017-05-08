d4231b74b1e78a2f3f9daf0611ce8635371f491c
Ibrahim Alkhatib <ibrahim_speedfire95@icloud.com>
2017-05-08 09:53:28 +0300

    Initial Commit

---
+++ Software Engineering/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Software Engineering
+//
+//  Created by ibrahim al-khatib on 5/8/17.
+//  Copyright © 2017 ibrahim al-khatib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
