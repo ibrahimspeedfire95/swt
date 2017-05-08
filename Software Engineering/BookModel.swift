e3a750436e0d3ff5740fee3b8ee18fd80c8bfcd2
Ibrahim Alkhatib <ibrahim_speedfire95@icloud.com>
2017-05-08 10:19:27 +0300

    asd

---
+++ Software Engineering/BookModel.swift
@@ -0,0 +1,15 @@
+//
+//  BookModel.swift
+//  Software Engineering
+//
+//  Created by ibrahim al-khatib on 5/8/17.
+//  Copyright © 2017 ibrahim al-khatib. All rights reserved.
+//
+
+import UIKit
+
+class BookModel: NSObject {
+    var name: String!
+    var author: String!
+    var pageCount: Int!
+}
--- Software Engineering/ViewController.swift
+++ Software Engineering/ViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    var books = [BookModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let book1 = BookModel()
+        book1.name = "Harry Potter"
+        book1.author = "Any"
+        book1.pageCount = 5
+        books.append(book1)
+        
+        let book2 = BookModel()
+        book2.name = "XYZ"
+        book2.author = "abc"
+        book2.pageCount = 4
+        books.append(book1)
+
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = books[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = model.name
+        cell?.detailTextLabel?.text = "\(model.pageCount)" +  ((model.pageCount == 1) ? "page" : "pages")
+        return cell!
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
